16467ec36aaafda9a260af2ae181e2d90669d21a
crazyoptimist <crazyoptimist@mail.com>
2021-09-11 08:10:32 -0500

    add winston logger

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 import express, { Application, Request, Response, NextFunction } from 'express'
+import Logger from './logger'
 
 const port = Number(process.env.PORT || 3000)
 
 const logRequest = (req: Request, _: Response, next: NextFunction) => {
   const { headers } = req
-  console.log(headers)
+  Logger.error('This is an error log')
+  Logger.warn('This is a warn log')
+  Logger.info('This is a info log')
+  Logger.http(`This is a http log:\n ${JSON.stringify(headers)}`)
+  Logger.debug('This is a debug log')
   next()
 }
 
