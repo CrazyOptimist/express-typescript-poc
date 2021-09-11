1b9bc8027eb033cd94562456c50e279df371fe1c
crazyoptimist <crazyoptimist@mail.com>
2021-09-11 07:48:55 -0500

    setup eslint, prettier, lint-staged, husky

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,20 @@
-import express, {Application, Request, Response, NextFunction} from 'express';
+import express, { Application, Request, Response, NextFunction } from 'express'
 
-const port = Number(process.env.PORT || 3000);
+const port = Number(process.env.PORT || 3000)
 
 const logRequest = (req: Request, _: Response, next: NextFunction) => {
   const { headers } = req
   console.log(headers)
-  next();
+  next()
 }
 
-const app: Application = express();
-app.use(logRequest);
+const app: Application = express()
+app.use(logRequest)
 
-app.get( "/", (_, res) => {
-    res.send( "Hello From Express With TypeScript" );
-});
+app.get('/', (_, res) => {
+  res.send('Hello From Express With TypeScript')
+})
 
 app.listen(port, () => {
-    console.log(`server started on port ${port}`);
-});
+  console.log(`server started on port ${port}`)
+})
